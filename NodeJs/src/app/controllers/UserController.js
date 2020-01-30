ab9d53d118a439a7cebf3be3c10154070ec226ff
Rodrigo Braga da Silva Santos <rodrigo.santos@sifra.local>
2020-01-30 09:23:52 -0300

    implementação models e controllers usuario

--- NodeJs/src/app/controllers/UserController.js
+++ NodeJs/src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Campos inválidos!' });
+    }
+
     const userExsist = await User.findOne({ where: { email: req.body.email } });
 
     if (userExsist) {
@@ -19,7 +34,48 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.status(400).json({ error: 'Sem token!' });
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Campos inválidos!' });
+    }
+
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email && email !== user.email) {
+      const userExsist = await User.findOne({ where: { email } });
+
+      if (userExsist) {
+        return res.status(400).json({ error: 'Usuário já existe!' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senha não existe!' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.status(200).json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
