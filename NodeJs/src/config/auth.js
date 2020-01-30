88186a05dcacfa8c2de3ae7c1874b31217e38c80
Rodrigo Santos <rsantosdesenv@hotmail.com>
2020-01-29 23:00:25 -0300

    inclusao de login e cadastro de usuários

---
+++ NodeJs/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'efcec6db284aa79781fdd5869f87fc98',
+  expiresIn: '7d',
+};
--- NodeJs/src/routes.js
+++ NodeJs/src/routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'Word' });
-});
+routes.post('/sessions', SessionController.store);
+routes.post('/user', UserController.store);
 
 export default routes;
