35c09ff5ebf8a8dc093713afa241f6a30cb34b73
Rodrigo Braga da Silva Santos <rodrigo.santos@sifra.local>
2020-03-04 14:21:31 -0300

    Implementação de redux e melhorias no layout.

--- ReactJs/src/pages/_layouts/auth/styles.js
+++ ReactJs/src/pages/_layouts/auth/styles.js
@@ -3,4 +3,67 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   height: 100vh;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+
+    span {
+      color: #f64c75;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #333;
+      font-weight: bold;
+      color: #fff;
+      border: 1px solid #333;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 0.85;
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
